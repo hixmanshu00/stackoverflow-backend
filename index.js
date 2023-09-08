7d230d5bddc18339a11d4e8bb178605388c25bb0
Himanshu Rajput <hiowner00@gmail.com>
2023-09-08 14:49:12 +0530

    added features

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ import cors from 'cors'
 import userRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
+import postRoutes from './routes/post.js'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+
+
 const app = express();
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb", extended: true}))
+
 
 app.use(cors());
 dotenv.config()
@@ -19,6 +24,7 @@ app.get('/',(req,res)=> {
 app.use('/user', userRoutes)
 app.use('/questions', questionRoutes)
 app.use('/answer', answerRoutes)
+app.use('/post', postRoutes)
 
 const PORT = process.env.PORT || 5000
 
@@ -27,3 +33,10 @@ const URL = process.env.CONNECTION_URL
 mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> {console.log(`server running on port ${PORT}`)}))
     .catch((err)=> console.log(err.message))
+
+
+app.use(bodyParser.json());
+
+
+
+    
